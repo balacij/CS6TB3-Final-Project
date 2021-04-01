a2025f7ece10629702899e32668099593b10386d
Jason Balaci <jasonbalaci@gmail.com>
2021-03-31 22:22:43 -0400

    quiet the cleaning script, and also clean locally

--- src/clean.sh
+++ src/clean.sh
@@ -1,2 +1,8 @@
-rm ./examples/*.wasm
-rm ./examples/*.wat
+# Being very precise with which directories we want to _actually_ clean, no file deletion mistakes allowed!
+
+rm ./examples/*.wasm 2> /dev/null
+rm ./examples/*.wat 2> /dev/null
+rm ./*.wasm 2> /dev/null
+rm ./*.wat 2> /dev/null
+
+echo 'All done! ✨ 🍰 ✨'
