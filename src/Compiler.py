8eab02921210f96552471f5fe5f232c31e80a24c
Jason Balaci <jasonbalaci@gmail.com>
2021-04-10 17:22:06 -0400

    Move runpywasm and runwasmer into Runtimes.y

--- src/Compiler.py
+++ src/Compiler.py
@@ -2,81 +2,6 @@ from P0 import compileString
 from ST import printSymTab
 
 
-def runpywasm(wasmFile):
-    import pywasm
-
-    def write(s, i):
-        print(i, end='')
-
-    def writeChar(s, i):
-        print(chr(i), end='')
-
-    def writeCharLn(s, i):
-        print(chr(i))
-
-    def writeln(s, i):
-        print(i)
-
-    def writeNewLine(s):
-        print()
-
-    def read(s):
-        return int(input())
-
-    vm = pywasm.load(
-        wasmFile,
-        {
-            "P0lib": {
-                "write": write,
-                "writeChar": writeChar,
-                "writeCharLn": writeCharLn,
-                "writeln": writeln,
-                "writeNewLine": writeNewLine,
-                "read": read,
-            }
-        },
-    )
-
-
-def runwasmer(wasmFile):
-    from wasmer import engine, Store, Module, Instance, ImportObject, Function
-    from wasmer_compiler_cranelift import Compiler
-
-    def write(i: int):
-        print(i, end='')
-
-    def writeChar(i: int):
-        print(chr(i), end='')
-
-    def writeCharLn(i: int):
-        print(chr(i))
-
-    def writeln(i: int):
-        print(i)
-
-    def writeNewLine():
-        print()
-
-    def read() -> int:
-        return int(input())
-
-    store = Store(engine.JIT(Compiler))
-    module = Module(store, open(wasmFile, 'rb').read())
-    import_object = ImportObject()
-    import_object.register(
-        "P0lib",
-        {
-            "write": Function(store, write),
-            "writeChar": Function(store, writeChar),
-            "writeCharLn": Function(store, writeCharLn),
-            "writeln": Function(store, writeln),
-            "writeNewLine": Function(store, writeNewLine),
-            "read": Function(store, read),
-        },
-    )
-    instance = Instance(module, import_object)
-
-
 def main(targetName, run=False, runtime='wasmer'):
     if not targetName.endswith('.p'):
         print('target file must end in ".p"')
@@ -97,6 +22,7 @@ def wat2wasmAndRun(targetName, runtime='wasmer'):
     wasmFile = targetName[:-4] + ".wasm"
     ec = os.system(f'wat2wasm "{targetName}" --output={wasmFile}')
     if ec == 0:
+        from Runtimes import runwasmer, runpywasm
         runner = None
         if runtime == 'wasmer':
             runner = runwasmer
---
+++ src/Runtimes.py
@@ -0,0 +1,74 @@
+
+def runpywasm(wasmFile):
+    import pywasm
+
+    def write(s, i):
+        print(i, end='')
+
+    def writeChar(s, i):
+        print(chr(i), end='')
+
+    def writeCharLn(s, i):
+        print(chr(i))
+
+    def writeln(s, i):
+        print(i)
+
+    def writeNewLine(s):
+        print()
+
+    def read(s):
+        return int(input())
+
+    vm = pywasm.load(
+        wasmFile,
+        {
+            "P0lib": {
+                "write": write,
+                "writeChar": writeChar,
+                "writeCharLn": writeCharLn,
+                "writeln": writeln,
+                "writeNewLine": writeNewLine,
+                "read": read,
+            }
+        },
+    )
+
+
+def runwasmer(wasmFile):
+    from wasmer import engine, Store, Module, Instance, ImportObject, Function
+    from wasmer_compiler_cranelift import Compiler
+
+    def write(i: int):
+        print(i, end='')
+
+    def writeChar(i: int):
+        print(chr(i), end='')
+
+    def writeCharLn(i: int):
+        print(chr(i))
+
+    def writeln(i: int):
+        print(i)
+
+    def writeNewLine():
+        print()
+
+    def read() -> int:
+        return int(input())
+
+    store = Store(engine.JIT(Compiler))
+    module = Module(store, open(wasmFile, 'rb').read())
+    import_object = ImportObject()
+    import_object.register(
+        "P0lib",
+        {
+            "write": Function(store, write),
+            "writeChar": Function(store, writeChar),
+            "writeCharLn": Function(store, writeCharLn),
+            "writeln": Function(store, writeln),
+            "writeNewLine": Function(store, writeNewLine),
+            "read": Function(store, read),
+        },
+    )
+    instance = Instance(module, import_object)
