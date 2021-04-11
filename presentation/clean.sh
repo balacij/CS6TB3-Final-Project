5c4460e963e6bf239fc501b558d5e1b07343780e
Jason Balaci <jasonbalaci@gmail.com>
2021-04-11 01:15:13 -0400

    another clean script

---
+++ presentation/clean.sh
@@ -0,0 +1,9 @@
+rm ./*.aux 2> /dev/null
+rm ./*.log 2> /dev/null
+rm ./*.nav 2> /dev/null
+rm ./*.out 2> /dev/null
+rm ./*.snm 2> /dev/null
+rm ./*.synctex.gz 2> /dev/null
+rm ./*.toc 2> /dev/null
+
+echo 'All done! ✨ 🍰 ✨'
