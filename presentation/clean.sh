9a72f10c6be3cad2fc2a04235a209c433fb49032
Jason Balaci <jasonbalaci@gmail.com>
2021-04-13 18:39:27 -0400

    clean up

--- presentation/clean.sh
+++ presentation/clean.sh
@@ -6,7 +6,7 @@ rm ./*.snm 2> /dev/null
 rm ./*.synctex.gz 2> /dev/null
 rm ./*.synctex\(busy\) 2> /dev/null
 rm ./*.toc 2> /dev/null
-rm -rf svg-inkscape 2> /dev/null
 rm ./*.vrb 2> /dev/null
+rm -rf svg-inkscape 2> /dev/null
 
 echo 'All done! ✨ 🍰 ✨'
